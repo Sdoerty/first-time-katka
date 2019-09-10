3b31fc3ad32362ccacd4861aa14529b41981ea32
CaptainMountain <romych1995@gmail.com>
2019-09-10 12:39:41 +0300

    first

---
+++ js/dropdown.js
@@ -0,0 +1,26 @@
+function dropNoti() {
+  var x = document.getElementById("dNoti");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function dropUser() {
+  var x = document.getElementById("dUser");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function dropSort() {
+  var x = document.getElementById("dSort");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
